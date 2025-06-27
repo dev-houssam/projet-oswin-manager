acf2f57331f7f68317deb05a3e9e28b216d4a8bd
SuperUseRYOS <42545976+dev-houssam@users.noreply.github.com>
2025-06-27 05:07:01 +0200

    Update lance.sh

--- lance.sh
+++ lance.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-npm start &
+# npm start &
+pm2 start src/app.js --name oswin-manager
+
+# Redémarrage entière du logiciel oswin-manager
+pm2 restart oswin-manager
+
+# Rechargement de la configuration de oswin-manager
+pm2 reload oswin-manager
+
+#Arreter temporairement l'execution de oswin-manager
+pm2 stop oswin-manager
+
+#Supprimer définitivement la gestion de oswin-manager
+pm2 delete oswin-manager
